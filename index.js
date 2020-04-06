b58f66e5a9c134eed2d87d264cd332dbe1f258b3
Guillaume Marcellin <marcellinguillaume@hotmail.fr>
2020-04-06 16:54:49 +0200

    Ajout du blinking au Players Name, création des 3 sections et des events listener sur les boutons de la home page, refacto des fonctions get first player et playerTurn pour les rendre compatibles avec le mode computer

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-let clickSound = document.getElementsByTagName("audio")[1];
-
 function main() {
-    getFirstPlayer()
     setInterval(setClickableCircles, 1000)
     setInterval(playerTurnText, 500)
+    setInterval(makeWinningPlayerBlink, 500)
 }
+
 
 main()
 
 
 //--------------------------------//
 
+computerButton = document.getElementById("computer-game-selection")
+twoPlayersButton = document.getElementById("two-players-game-selection")
 
+computerButton.onclick = (e) => {
+    document.querySelector(".main-menu").classList.add("hidden")
+    document.querySelector(".computer-game").classList.remove("hidden")
+    let firstPlayer = Math.floor(Math.random() * 2)
+    if (firstPlayer) {
+        document.getElementById("computer").classList.add("player-1")
+    } else {
+        document.getElementById("player").classList.add("player-2")
+    }
+}
 
-function getFirstPlayer() {
+twoPlayersButton.onclick = (e) => {
+    document.querySelector(".main-menu").classList.add("hidden")
+    document.querySelector(".two-players-game").classList.remove("hidden")
     let firstPlayer = Math.floor(Math.random() * 2)
     if (firstPlayer) {
         document.getElementById("player1").classList.add("player-1")
@@ -23,21 +36,29 @@ function getFirstPlayer() {
 
 }
 
+
 function playerTurnText() {
-    if (document.getElementById("player1").className.includes("player-1")) {
-        document.querySelector(".player-turn").textContent = `It's player 1's Turn`
-    } else {
-        document.querySelector(".player-turn").textContent = `It's player 2's Turn`
+    if (!checkConnectFour()) {
+        if (document.getElementById("player1").className.includes("player-1")) {
+            document.querySelector(".player-turn").textContent = `It's player 1's Turn`
+        } else if (document.getElementById("player2").className.includes("player-2")) {
+            document.querySelector(".player-turn").textContent = `It's player 2's Turn`
+        } else if (document.getElementById("computer").className.includes("player-1")) {
+            document.querySelector(".player-turn.computer").textContent = `It's Computer's Turn`
+        } else {
+            document.querySelector(".player-turn.computer").textContent = `It's Player's Turn`
+        }
     }
+
 }
 
-const circlesColumnOne = document.querySelectorAll("div.container :nth-child(7n+1)")
-const circlesColumnTwo = document.querySelectorAll("div.container :nth-child(7n+2)")
-const circlesColumnThree = document.querySelectorAll("div.container :nth-child(7n+3)")
-const circlesColumnFour = document.querySelectorAll("div.container :nth-child(7n+4)")
-const circlesColumnFive = document.querySelectorAll("div.container :nth-child(7n+5)")
-const circlesColumnSix = document.querySelectorAll("div.container :nth-child(7n+6)")
-const circlesColumnSeven = document.querySelectorAll("div.container :nth-child(7n+7)")
+const circlesColumnOne = document.querySelectorAll("div.grid :nth-child(7n+1)")
+const circlesColumnTwo = document.querySelectorAll("div.grid :nth-child(7n+2)")
+const circlesColumnThree = document.querySelectorAll("div.grid :nth-child(7n+3)")
+const circlesColumnFour = document.querySelectorAll("div.grid :nth-child(7n+4)")
+const circlesColumnFive = document.querySelectorAll("div.grid :nth-child(7n+5)")
+const circlesColumnSix = document.querySelectorAll("div.grid :nth-child(7n+6)")
+const circlesColumnSeven = document.querySelectorAll("div.grid :nth-child(7n+7)")
 
 const circlesRowOne = document.querySelectorAll(".row1")
 const circlesRowTwo = document.querySelectorAll(".row2")
@@ -52,14 +73,14 @@ const allCircles = document.querySelectorAll(".item")
 function getClickableCircleByColumn(column) {
     let clickableCircle = ""
 
-    if (!column[6].className.includes("redChip") && !column[6].className.includes("greenChip")) {
+    if (!column[6].className.includes("purpleChip") && !column[6].className.includes("greenChip")) {
         return column[6]
-    } else if (column[0].className.includes("redChip") || column[0].className.includes("greenChip")) {
+    } else if (column[0].className.includes("purpleChip") || column[0].className.includes("greenChip")) {
         return 0
     } else {
         for (let i = 1; i < 7; i++) {
 
-            if (column[i].className.includes("redChip") || column[i].className.includes("greenChip")) {
+            if (column[i].className.includes("purpleChip") || column[i].className.includes("greenChip")) {
                 clickableCircle = column[i - 1]
                 break;
             }
@@ -69,17 +90,19 @@ function getClickableCircleByColumn(column) {
     return clickableCircle
 }
 
+let clickSound = document.getElementsByTagName("audio")[1];
+
 function setClickEventListener(x) {
     if (x != 0) {
         x.onclick = (e) => {
             if (document.getElementById("player1").className.includes("player-1") && !e.target.className.includes("greenChip")) {
-                e.target.classList.add("redChip")
+                e.target.classList.add("purpleChip")
                 document.getElementById("player1").classList.remove("player-1")
                 document.getElementById("player2").classList.add("player-2")
                 checkConnectFour()
                 clickSound.play()
                 return 0
-            } else if (!e.target.className.includes("redChip")) {
+            } else if (!e.target.className.includes("purpleChip")) {
                 e.target.classList.add("greenChip")
                 document.getElementById("player2").classList.remove("player-2")
                 document.getElementById("player1").classList.add("player-1")
@@ -113,7 +136,7 @@ function setClickableCircles() {
 
 function checkConnectFourByColumn(column) {
     for (let i = 0; i < 4; i++) {
-        if ((column[i].className.includes("redChip") && column[i + 1].className.includes("redChip") && column[i + 2].className.includes("redChip") && column[i + 3].className.includes("redChip")) || (column[i].className.includes("greenChip") && column[i + 1].className.includes("greenChip") && column[i + 2].className.includes("greenChip") && column[i + 3].className.includes("greenChip"))) {
+        if ((column[i].className.includes("purpleChip") && column[i + 1].className.includes("purpleChip") && column[i + 2].className.includes("purpleChip") && column[i + 3].className.includes("purpleChip")) || (column[i].className.includes("greenChip") && column[i + 1].className.includes("greenChip") && column[i + 2].className.includes("greenChip") && column[i + 3].className.includes("greenChip"))) {
             column[i].classList.add("blinking")
             column[i + 1].classList.add("blinking")
             column[i + 2].classList.add("blinking")
@@ -125,7 +148,7 @@ function checkConnectFourByColumn(column) {
 
 function checkConnectFourByRow(row) {
     for (let i = 0; i < 4; i++) {
-        if ((row[i].className.includes("redChip") && row[i + 1].className.includes("redChip") && row[i + 2].className.includes("redChip") && row[i + 3].className.includes("redChip")) || (row[i].className.includes("greenChip") && row[i + 1].className.includes("greenChip") && row[i + 2].className.includes("greenChip") && row[i + 3].className.includes("greenChip"))) {
+        if ((row[i].className.includes("purpleChip") && row[i + 1].className.includes("purpleChip") && row[i + 2].className.includes("purpleChip") && row[i + 3].className.includes("purpleChip")) || (row[i].className.includes("greenChip") && row[i + 1].className.includes("greenChip") && row[i + 2].className.includes("greenChip") && row[i + 3].className.includes("greenChip"))) {
             row[i].classList.add("blinking")
             row[i + 1].classList.add("blinking")
             row[i + 2].classList.add("blinking")
@@ -138,7 +161,7 @@ function checkConnectFourByRow(row) {
 function checkConnectFourByDiagonal() {
     for (let j = 0; j < 22; j += 7) {
         for (let i = 0; i < 4; i++) {
-            if ((allCircles[i + j].className.includes("redChip") && allCircles[i + j + 8].className.includes("redChip") && allCircles[i + j + 16].className.includes("redChip") && allCircles[i + j + 24].className.includes("redChip")) || (allCircles[i + j].className.includes("greenChip") && allCircles[i + j + 8].className.includes("greenChip") && allCircles[i + j + 16].className.includes("greenChip") && allCircles[i + j + 24].className.includes("greenChip"))) {
+            if ((allCircles[i + j].className.includes("purpleChip") && allCircles[i + j + 8].className.includes("purpleChip") && allCircles[i + j + 16].className.includes("purpleChip") && allCircles[i + j + 24].className.includes("purpleChip")) || (allCircles[i + j].className.includes("greenChip") && allCircles[i + j + 8].className.includes("greenChip") && allCircles[i + j + 16].className.includes("greenChip") && allCircles[i + j + 24].className.includes("greenChip"))) {
                 allCircles[i + j].classList.add("blinking")
                 allCircles[i + j + 8].classList.add("blinking")
                 allCircles[i + j + 16].classList.add("blinking")
@@ -149,7 +172,7 @@ function checkConnectFourByDiagonal() {
     }
     for (let j = 0; j < 22; j += 7) {
         for (let i = 6; i > 2; i--) {
-            if ((allCircles[i + j].className.includes("redChip") && allCircles[i + j + 6].className.includes("redChip") && allCircles[i + j + 12].className.includes("redChip") && allCircles[i + j + 18].className.includes("redChip")) || (allCircles[i + j].className.includes("greenChip") && allCircles[i + j + 6].className.includes("greenChip") && allCircles[i + j + 12].className.includes("greenChip") && allCircles[i + j + 18].className.includes("greenChip"))) {
+            if ((allCircles[i + j].className.includes("purpleChip") && allCircles[i + j + 6].className.includes("purpleChip") && allCircles[i + j + 12].className.includes("purpleChip") && allCircles[i + j + 18].className.includes("purpleChip")) || (allCircles[i + j].className.includes("greenChip") && allCircles[i + j + 6].className.includes("greenChip") && allCircles[i + j + 12].className.includes("greenChip") && allCircles[i + j + 18].className.includes("greenChip"))) {
                 allCircles[i + j].classList.add("blinking")
                 allCircles[i + j + 6].classList.add("blinking")
                 allCircles[i + j + 12].classList.add("blinking")
@@ -171,6 +194,24 @@ function checkConnectFour() {
         return true
     } else {
         return false
+    }
+
+}
+
+function makeWinningPlayerBlink() {
+
+    if (checkConnectFour()) {
+        if (document.getElementById("player1").className.includes("player-1")) {
+            document.getElementById("player2").classList.add("blinking")
+            document.getElementById("player2").classList.add("winning-background")
+            document.getElementById("player1").classList.remove("player-1")
+            document.querySelector(".player-turn").textContent = `Player 2 Wins`
+        } else if (document.getElementById("player2").className.includes("player-2")) {
+            document.getElementById("player1").classList.add("blinking")
+            document.getElementById("player1").classList.add("winning-background")
+            document.getElementById("player2").classList.remove("player-2")
+            document.querySelector(".player-turn").textContent = `Player 1 Wins`
+        }
     }
 
 }
